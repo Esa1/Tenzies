1c00fee17f3d8abeba5d9567d5032b7564f0510e
Esa Torkkila <esa.torkkila@gmail.com>
2022-06-03 11:32:39 +0300

    Die components. Change dice to objects

--- src/App.js
+++ src/App.js
@@ -1,47 +1,45 @@
 import React, { useState } from "react";
 import Die from "./Components/Die"
+import {nanoid} from "nanoid"
 
+export default function App() {
 /**
- * Challenge: Create a `Roll Dice` button that will re-roll
- * all 10 dice
+ * Challenge: Update the array of numbers in state to be
+ * an array of objects instead. Each object should look like:
+ * { value: <random number>, isHeld: false }
  * 
- * Clicking the button should generate a new array of numbers
- * and set the `dice` state to that new array (thus re-rendering
- * the array to the page)
+ * Making this change will break parts of our code, so make
+ * sure to update things so we're back to a working state
  */
 
- export default function App() {
-    /**
-     * Challenge: Create a `Roll Dice` button that will re-roll
-     * all 10 dice
-     * 
-     * Clicking the button should generate a new array of numbers
-     * and set the `dice` state to that new array (thus re-rendering
-     * the array to the page)
-     */
+    const [dice, setDice] = React.useState(allNewDice())
     
-        const [dice, setDice] = React.useState(allNewDice())
-        
-        function allNewDice() {
-            const newDice = []
-            for (let i = 0; i < 10; i++) {
-                newDice.push(Math.ceil(Math.random() * 6))
-            }
-            return newDice
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push({
+                value: Math.ceil(Math.random() * 6), 
+                isHeld: false,
+                id: nanoid()
+            })
         }
-        
-        function rollDice() {
-            setDice(allNewDice())
-        }
-        
-        const diceElements = dice.map(die => <Die value={die} />)
-        
-        return (
-            <main>
-                <div className="dice-container">
-                    {diceElements}
-                </div>
-                <button className="roll-dice" onClick={rollDice}>Roll</button>
-            </main>
-        )
+        return newDice
     }
+    
+    function rollDice() {
+        setDice(allNewDice())
+    }
+    
+    const diceElements = dice.map(die => (
+        <Die key={die.id} value={die.value} />
+    ))
+    
+    return (
+        <main>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
+        </main>
+    )
+}
