0a2bcdeb699a94a20f47c1a2912c82aba9e4618e
Esa Torkkila <esa.torkkila@gmail.com>
2022-06-09 10:11:28 +0300

    Die components. Hold dice part !

--- src/App.js
+++ src/App.js
@@ -3,43 +3,57 @@ import Die from "./Components/Die"
 import {nanoid} from "nanoid"
 
 export default function App() {
-/**
- * Challenge: Add conditional styling to the Die component
- * so that if it's held (isHeld === true), its background color
- * changes to a light green (#59E391)
- * 
- * Remember: currently the Die component has no way of knowing
- * if it's "held" or not.
- */
-
-    const [dice, setDice] = React.useState(allNewDice())
+    /**
+     * Challenge: Create a function `holdDice` that takes
+     * `id` as a parameter. For now, just have the function
+     * console.log(id).
+     * 
+     * Then, figure out how to pass that function down to each
+     * instance of the Die component so when each one is clicked,
+     * it logs its own unique ID property. (Hint: there's more
+     * than one way to make that work, so just choose whichever
+     * you want)
+     * 
+     */
     
-    function allNewDice() {
-        const newDice = []
-        for (let i = 0; i < 10; i++) {
-            newDice.push({
-                value: Math.ceil(Math.random() * 6), 
-                isHeld: false,
-                id: nanoid()
-            })
+        const [dice, setDice] = React.useState(allNewDice())
+        
+        function holdDice(id) {
+            console.log(id)
         }
-        return newDice
+
+        function allNewDice() {
+            const newDice = []
+            for (let i = 0; i < 10; i++) {
+                newDice.push({
+                    value: Math.ceil(Math.random() * 6),
+                    isHeld: false,
+                    id: nanoid()
+                })
+            }
+            return newDice
+        }
+        
+        function rollDice() {
+            setDice(allNewDice())
+        }
+        
+        const diceElements = dice.map(die => (
+            <Die 
+                key={die.id} 
+                id={die.id} 
+                value={die.value} 
+                isHeld={die.isHeld}
+                holdDice={holdDice} 
+            />
+        ))
+        
+        return (
+            <main>
+                <div className="dice-container">
+                    {diceElements}
+                </div>
+                <button className="roll-dice" onClick={rollDice}>Roll</button>
+            </main>
+        )
     }
-    
-    function rollDice() {
-        setDice(allNewDice())
-    }
-    
-    const diceElements = dice.map(die => (
-        <Die key={die.id} value={die.value} isHeld={die.isHeld}/>
-    ))
-    
-    return (
-        <main>
-            <div className="dice-container">
-                {diceElements}
-            </div>
-            <button className="roll-dice" onClick={rollDice}>Roll</button>
-        </main>
-    )
-}
