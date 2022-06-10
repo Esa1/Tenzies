54eaf2a0b55a67e5162810eae9a7c5cbfa5d7a07
Esa Torkkila <esa.torkkila@gmail.com>
2022-06-10 04:44:24 +0300

    Die components. Hold dice part 2, Bob's version

--- src/App.js
+++ src/App.js
@@ -24,28 +24,25 @@ export default function App() {
         setDice(allNewDice())
     }
     
-/**
- * Challenge: Update the `holdDice` function to flip
- * the `isHeld` property on the object in the array
- * that was clicked, based on the `id` prop passed
- * into the function.
- * 
- * Hint: as usual, there's > 1 way to accomplish this.
- * I'll be using `dice.map()` and checking for the `id`
- * of the die to determine which one to flip `isHeld` on,
- * but you can do whichever way makes the most sense to you.
- */
+    /**
+     * Challenge: Update the `holdDice` function to flip
+     * the `isHeld` property on the object in the array
+     * that was clicked, based on the `id` prop passed
+     * into the function.
+     * 
+     * Hint: as usual, there's > 1 way to accomplish this.
+     * I'll be using `dice.map()` and checking for the `id`
+     * of the die to determine which one to flip `isHeld` on,
+     * but you can do whichever way makes the most sense to you.
+     */
     function holdDice(id) {
-        console.log(id)
-        const newDice = dice.map(d => {
-            if (d.id === id)
-                return {...d, isHeld: true}
-            else
-                return d
-        })
-        setDice(newDice)
+        setDice(oldDice => oldDice.map(die => {
+            return die.id === id ? 
+                {...die, isHeld: !die.isHeld} :
+                die
+        }))
     }
-    
+
     const diceElements = dice.map(die => (
         <Die 
             key={die.id} 
