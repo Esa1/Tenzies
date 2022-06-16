501613d4dc141055afe73dead9b706a9407ac733
Esa Torkkila <esa.torkkila@gmail.com>
2022-06-16 08:21:43 +0300

    Add confetti

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Die from "./Components/Die"
 import {nanoid} from "nanoid"
+import Confetti from 'react-confetti'
 
 export default function App() {
 
     const [dice, setDice] = React.useState(allNewDice())
     const [tenzies, setTenzies] = React.useState(false)
-    console.log(tenzies)
+    
     React.useEffect(() => {
         const allHeld = dice.every(die => die.isHeld)
         const firstValue = dice[0].value
@@ -70,13 +71,17 @@ export default function App() {
     
     return (
         <main>
+            {tenzies && <Confetti />}
             <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
                 {diceElements}
             </div>
-            <button className="roll-dice" onClick={rollDice}>
-                {tenzies ? "New game" : "Roll"}
+            <button 
+                className="roll-dice" 
+                onClick={rollDice}
+            >
+                {tenzies ? "New Game" : "Roll"}
             </button>
         </main>
     )
